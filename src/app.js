b1a85fd7bb6cfa532f316b97c510bf207c920b5e
willnickerson <wnickers@willamette.edu>
2017-02-20 11:55:06 -0800

    basic slider animations working

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import uiRouter from 'angular-ui-router';
 import defaultRoute from 'angular-ui-router-default';
 import routes from './routes';
 import duScroll from 'angular-scroll';
+import TweenMax from 'gsap';
 // import ngAnimate from 'angular-animate';
 //TODO: figure out why things broken when a imported angular animate in the es6y way
 
@@ -27,3 +28,29 @@ app.value('apiUrl', dev);
 app.factory('apiUrl', function() {
     return dev;
 });
+
+app.animation('.slide-animation', function ($window) {
+    return {
+        addClass: function (element, className, done) {
+            if (className == 'ng-hide') {
+                // console.log('element', element.parent());
+                console.log('$window', $window.innerWidth);
+                TweenMax.to(element, 0.3, {left: $window.innerWidth, onComplete: done });
+            }
+            else {
+                done();
+            }
+        },
+        removeClass: function (element, className, done) {
+            if (className == 'ng-hide') {
+                // console.log('element', element.parent());
+                element.removeClass('ng-hide');
+                TweenMax.set(element, { left: $window.innerWidth });
+                TweenMax.to(element, 0.3, {left: 0, onComplete: done });
+            }
+            else {
+                done();
+            }
+        }
+    };
+}); 
