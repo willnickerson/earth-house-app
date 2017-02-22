ce8ff80e48105c2a58944d555625a4726a8563ac
willnickerson <wnickers@willamette.edu>
2017-02-21 16:10:17 -0800

    accent color and styling added to links and image slider

--- src/components/hero-image/hero-image.js
+++ src/components/hero-image/hero-image.js
@@ -13,17 +13,17 @@ function controller() {
         {
             name: 'img1',
             imgUrl: 'http://res.cloudinary.com/lejipni8p/image/upload/v1482867043/earth%20house/wood-spread-1_cda0uc.jpg',
-            text: 'Sed ut perspiciatis omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo. <span>follow us on instagram!</span>'
+            text: 'Sed ut perspiciatis omnis iste natus error sit voluptatem accusantium doloremque laudantium. <a>follow us on instagram!</a>'
         },
         {
             name: 'img2',
             imgUrl: 'http://res.cloudinary.com/lejipni8p/image/upload/v1487379385/IMG_7888.tiff_mkn1dh.jpg',
-            text: 'Sed sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo.'
+            text: 'Sed sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium. <a>Read about our juices</a>'
         },
         {
             name: 'img3',
             imgUrl: 'http://res.cloudinary.com/lejipni8p/image/upload/v1487379374/IMG_1125.tiff_dxfsmv.jpg',
-            text: 'Sed sed sedut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo.'
+            text: 'Sed sed sedut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam <a>Check out our online store</a>'
         }
     ];
 
