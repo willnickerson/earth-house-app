ef86658d797738a90826a9d828cd181515282831
willnickerson <wnickers@willamette.edu>
2017-02-28 15:42:49 -0800

    double frame removed for articles

--- src/components/hero-image/hero-image.js
+++ src/components/hero-image/hero-image.js
@@ -16,19 +16,19 @@ function controller($window, $interval) {
     this.slides = [
         {
             name: 'img1',
-            imgUrl: 'http://res.cloudinary.com/lejipni8p/image/upload/c_crop,g_south,h_2100,w_3750/v1487806715/platter-cropped_ntr56a.jpg',
+            imgUrl: 'http://res.cloudinary.com/lejipni8p/image/upload/v1488324606/sumpreme-green-centered_wp639t.jpg',
             // text: 'Sed ut perspiciatis omnis iste natus error sit voluptatem accusantium doloremque laudantium. <a href="https://www.instagram.com">follow us on instagram!</a>'
             text: '<a>Supreme Green</a>'
         },
         {
             name: 'img2',
-            imgUrl: 'http://res.cloudinary.com/lejipni8p/image/upload/c_crop,g_south,h_2285,w_4024/v1487807738/platter3-cropped_urspmq.jpg',
+            imgUrl: 'http://res.cloudinary.com/lejipni8p/image/upload/v1488324567/IMG_1125.tiff_mt0jkc.jpg',
             // text: 'Sed sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium. <a>Read about our juices</a>'
             text: '<a>Pear-adise</a>'
         },
         {
             name: 'img3',
-            imgUrl: 'http://res.cloudinary.com/lejipni8p/image/upload/c_crop,g_south,w_3750/v1487807406/platter2-cropped_t3v2cx.jpg',
+            imgUrl: 'http://res.cloudinary.com/lejipni8p/image/upload/v1488324567/IMG_1125.tiff_mt0jkc.jpg',
             text: '<a>Tranquil Fennel</a>'
             // text: 'Sit voluptatem accusantium doloremque laudantium, totam rem aperiam <a ui-sref="shop">Check out our online store</a>'
         }
