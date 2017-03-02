8630ff383e0131c80a713b8abe757585a5078f82
willnickerson <wnickers@willamette.edu>
2017-03-01 18:04:35 -0800

    another failed attempt to make a loading bar for the carousel

--- src/components/articles/articles.js
+++ src/components/articles/articles.js
@@ -3,8 +3,19 @@ import styles from './articles.scss';
 
 export default {
     template,
-    controller() {
-        this.styles = styles;
-    }
+    controller
 };
+
+controller.$inject = ['$interval'];
+
+function controller($interval) {
+    this.styles = styles;
+
+    this.loading = this.styles.done;
+
+    $interval(() => {
+        console.log(this.loading);
+        this.loading = this.styles.start;
+    }, 4000);
+}
 
