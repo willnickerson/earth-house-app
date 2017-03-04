c3f5c9ce9c2c66c053ea9e83f1b4ebcae67a5c88
willnickerson <wnickers@willamette.edu>
2017-03-03 17:11:13 -0800

    button and select added to determine number of items to add to cart

--- src/components/shop/all/all.js
+++ src/components/shop/all/all.js
@@ -5,5 +5,20 @@ export default {
     bindings: {
         juices: '<',
         gotoItems: '<'
-    }
+    },
+    controller
 };
+
+function controller() {
+    this.selectArray = [];
+
+    for(var i = 0; i < 10; i++) {
+        this.selectArray.push(i + 1);
+    }
+
+    this.quantity = 0;
+
+    this.addToCart = function(quantity) {
+        console.log('add to cart', quantity);
+    };
+}
