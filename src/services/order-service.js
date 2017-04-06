0f9dedd8e1462be48244a793fee8cbe7f3a0ac49
willnickerson <wnickers@willamette.edu>
2017-04-06 11:41:25 -0700

    order service now works and requires auth

--- src/services/order-service.js
+++ src/services/order-service.js
@@ -3,12 +3,15 @@ orderService.$inject = ['$http', 'apiUrl'];
 export default function orderService($http, apiUrl) {
     return {
         getOrders(token) {
-            $http({
+            return $http({
                 method: 'GET',
                 url: `${apiUrl}/orders`,
                 headers: {
                     'Authorization': token
                 }
+            }).then(res => {
+                console.log('In service', res.data);
+                return res.data;
             });
         }
     };
