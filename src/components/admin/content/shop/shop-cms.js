5961f14e00c02915a50887d4d2212ee314ebe85e
willnickerson <wnickers@willamette.edu>
2017-04-18 19:59:58 -0700

    all of the ingredient service methods are accesible from the cms portal

--- src/components/admin/content/shop/shop-cms.js
+++ src/components/admin/content/shop/shop-cms.js
@@ -14,24 +14,54 @@ function controller(ingredientService) {
     this.$onInit = () => {
         ingredientService.getAll()
             .then(ingredients => {
-                ingredients.sort((curr, next) => {
-                    const currName = curr.name.toUpperCase();
-                    const nextName = next.name.toUpperCase();
+                // ingredients.sort((curr, next) => {
+                //     const currName = curr.name.toUpperCase();
+                //     const nextName = next.name.toUpperCase();
 
-                    if(currName < nextName) {
-                        return -1;
-                    }
-                    if(currName > nextName) {
-                        return 1;
-                    }
-                    return 0;
-                });
+                //     if(currName < nextName) {
+                //         return -1;
+                //     }
+                //     if(currName > nextName) {
+                //         return 1;
+                //     }
+                //     return 0;
+                // });
+                alphabetize(ingredients);
                 this.ingredients = ingredients;
             });
+    };
+
+    const alphabetize = arr => {
+        arr.sort((curr, next) => {
+            const currName = curr.name.toUpperCase();
+            const nextName = next.name.toUpperCase();
+
+            if(currName < nextName) {
+                return -1;
+            }
+            if(currName > nextName) {
+                return 1;
+            }
+            return 0;
+        });
     };
 
     this.delete = ingredient => {
         ingredientService.delete(ingredient._id, this.token);
         this.ingredients.splice(this.ingredients.indexOf(ingredient), 1);
+    };
+
+    this.update = ingredient => {
+        ingredientService.update(ingredient, ingredient._id, this.token)
+            .then(updated => console.log(updated));
+    };
+
+    this.add = ingredient => {
+        ingredientService.create(ingredient, this.token)
+            .then(saved => {
+                ingredient = {};
+                this.ingredients.push(saved);
+                alphabetize(this.ingredients);
+            });
     };
 }
