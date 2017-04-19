2cd18c1c1a43ae7fc63d70acaef30ea922086730
willnickerson <wnickers@willamette.edu>
2017-04-18 19:31:57 -0700

    ingredient service built and ingredients can be deleted through the cms portal

--- src/components/admin/content/content.js
+++ src/components/admin/content/content.js
@@ -15,6 +15,8 @@ function controller($state) {
     this.$onInit = () => {
         if(!this.token) $state.go('admin.login');
     };
+    //delete 
+    this.token= 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU4ZTU2YTg0ZTMwNDI5MDdkODkyNzVkYyIsInVzZXJuYW1lIjoidGVzdCIsImlhdCI6MTQ5MjU2ODIwM30.Qb6NYuJGngCE-Y6jHQV9_4hW2gt2_ziWz7aO2Cpkff8';
     
     this.contentToManage = 'about';
 
--- src/components/admin/content/shop/shop-cms.js
+++ src/components/admin/content/shop/shop-cms.js
@@ -1,5 +1,37 @@
 import template from './shop-cms.html';
 
 export default({
-    template
+    template,
+    bindings: {
+        token: '<'
+    },
+    controller
 });
+
+controller.$inject = ['ingredientService'];
+
+function controller(ingredientService) {
+    this.$onInit = () => {
+        ingredientService.getAll()
+            .then(ingredients => {
+                ingredients.sort((curr, next) => {
+                    const currName = curr.name.toUpperCase();
+                    const nextName = next.name.toUpperCase();
+
+                    if(currName < nextName) {
+                        return -1;
+                    }
+                    if(currName > nextName) {
+                        return 1;
+                    }
+                    return 0;
+                });
+                this.ingredients = ingredients;
+            });
+    };
+
+    this.delete = ingredient => {
+        ingredientService.delete(ingredient._id, this.token);
+        this.ingredients.splice(this.ingredients.indexOf(ingredient), 1);
+    };
+}
