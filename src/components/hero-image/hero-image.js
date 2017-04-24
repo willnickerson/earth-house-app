85770a15d84be0b96476e670ade51341c8a45cbc
willnickerson <wnickers@willamette.edu>
2017-04-24 14:32:49 -0700

    now the hero image component randomly displays three not repeating slides from the array of visible slides

--- src/components/hero-image/hero-image.js
+++ src/components/hero-image/hero-image.js
@@ -14,11 +14,9 @@ controller.$inject = ['$window', '$interval'];
 
 function controller($window, $interval) {
     this.$onInit = () => {
-        console.log('in on init: ', this);
+        this.slidesVisible = [];
         if(this.slides.length < 3) {
-            console.log('in if');
-            this.slides = [
-                //hard coded slides if not enough slides in DB
+            this.slidesVisible = [
                 {
                     name: 'img3',
                     imgUrl: 'http://res.cloudinary.com/lejipni8p/image/upload/v1488324567/IMG_1125.tiff_mt0jkc.jpg',
@@ -38,16 +36,24 @@ function controller($window, $interval) {
                     link: 'https://earth-house.herokuapp.com/#!/shop/item/58c85e66f36d287eb5cb74ef'
                 }
             ];
+        } else {
+            do {
+                const index = Math.floor(Math.random() * this.slides.length);
+                if(this.slidesVisible.indexOf(this.slides[index]) === -1) {
+                    console.log('in if: ', index);
+                    this.slidesVisible.push(this.slides[index]);
+                }
+            } while (this.slidesVisible.length < 3);
         }
-        console.log('slides: ', this.slides);
+        console.log('slides: ', this.slidesVisible);
         this.currIndex = 0;
-        this.currText = this.slides[0].text;    
+        this.currText = this.slidesVisible[0].text;    
     };
     this.styles = styles;
 
     
     this.setCurrText = function() {
-        this.currText = this.slides[this.currIndex].text;
+        this.currText = this.slidesVisible[this.currIndex].text;
     };
 
     this.setCurrIndex = function(index) {
@@ -61,14 +67,12 @@ function controller($window, $interval) {
     };
 
     this.nextSlide = function() {
-        // this.progress = this.styles.finish;
-        // console.log(this.progress);
-        this.currIndex = (this.currIndex < this.slides.length - 1) ? ++this.currIndex : 0;
+        this.currIndex = (this.currIndex < this.slidesVisible.length - 1) ? ++this.currIndex : 0;
         this.setCurrText();
     };
 
     this.prevSlide = function() {
-        this.currIndex = (this.currIndex > 0) ? --this.currIndex : this.slides.length -1;
+        this.currIndex = (this.currIndex > 0) ? --this.currIndex : this.slidesVisible.length -1;
         this.setCurrText();
     };
 
