41ba23e9d1d39797bceb310a638094479b08fb28
willnickerson <wnickers@willamette.edu>
2017-05-09 13:01:58 -0700

    created component for  the home page to join mail list

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,11 +1,18 @@
 import template from './home.html';
+import styles from './home.scss';
 
 export default {
     template,
     bindings: {
         slides: '<',
         articles: '<'
-    }
+    },
+    controller
 };
+
+
+function controller() {
+    this.styles = styles;
+}
 
 
---
+++ src/components/mail-list/mail-list.js
@@ -0,0 +1,11 @@
+import template from './mail-list.html';
+import styles from './mail-list.scss';
+
+export default({
+    template,
+    controller
+});
+
+function controller() {
+    this.styles = styles;
+}
